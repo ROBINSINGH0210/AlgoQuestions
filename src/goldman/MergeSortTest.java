9c096de86df3fff2288afaef9426e8bf6e846578
ROBIN SINGH <robinsingh0210@gmail.com>
2018-07-02 15:04:07 +0530

    Sorting Alog

---
+++ src/goldman/MergeSortTest.java
@@ -0,0 +1,50 @@
+package goldman;
+
+public class MergeSortTest {
+
+	public static void main(String[] args) {
+		int arr[] = { 4, 5, 1, 2 ,34,4,121,4,12,111};
+
+		for (int i : mergeSort(arr, 0, arr.length - 1, new int[arr.length])) {
+			System.out.println(i);
+		}
+
+	}
+
+	static int[] mergeSort(int arr[], int start, int end, int temp[]) {
+		if (start >= end) {
+			return arr;
+		}
+		int mid = (start + end) / 2;
+		mergeSort(arr, start, mid, temp);
+		mergeSort(arr, mid + 1, end, temp);
+		merge(arr, start, mid, end, temp);
+		return arr;
+	}
+
+	static void merge(int[] arr, int start, int mid, int end, int[] temp) {
+		for (int i = start; i <= end; i++) {
+			temp[i] = arr[i];
+		}
+		int i = start;
+		int j = mid + 1;
+		int k = start;
+		while (i <= j && j <= end) {
+			if (temp[i] <= temp[j]) {
+				arr[k] = temp[i];
+				i++;
+			} else {
+				arr[k] = temp[j];
+				j++;
+			}
+			k++;
+		}
+		while (i <= mid) {
+			arr[k] = temp[i];
+			i++;
+			k++;
+		}
+
+	}
+
+}
