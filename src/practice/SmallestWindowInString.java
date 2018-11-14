3f9143ee83cb57450594628ce4f1c3e85fa6ac19
ROBIN SINGH <robinsingh0210@gmail.com>
2018-11-14 18:01:55 +0530

    Updated the code.

--- src/practice/SmallestWindowInString.java
+++ src/practice/SmallestWindowInString.java
@@ -14,36 +14,36 @@ public class SmallestWindowInString {
 		for (Character character : sub.toCharArray()) {
 			strSet.add(character);
 		}
-		int firstSo = -1;
-		int lastSofar = -1;
+		int first = -1;
+		int lastSo = -1;
 		int diffd = Integer.MAX_VALUE;
 		int start = 0;
 		while (start < str.length()) {
-			int first = -1;
-			int last = -1;
-			int diff = -1;
+			int tempFirst = -1;
+			int tempLast = -1;
+			int tempDiff = -1;
 			List<Character> s = new ArrayList<Character>(strSet);
 			for (int i = start; i < str.length() && !s.isEmpty(); i++) {
 				if (s.remove(Character.valueOf(str.charAt(i)))) {
-					if (first < 0)
-						first = i;
+					if (tempFirst < 0)
+						tempFirst = i;
 					else
-						last = i;
-					diff = last - first;
+						tempLast = i;
+					tempDiff = tempLast - tempFirst;
 				}
 			}
 
 			if (s.isEmpty()) {
-				if (diffd > diff) {
-					firstSo = first;
-					lastSofar = last;
-					diffd = diff;
+				if (diffd > tempDiff) {
+					first = tempFirst;
+					lastSo = tempLast;
+					diffd = tempDiff;
 				}
 			}
 			start++;
 		}
-		if (firstSo > 0 && lastSofar > 0) {
-			return str.substring(firstSo, lastSofar + 1);
+		if (first > 0 && lastSo > 0) {
+			return str.substring(first, lastSo + 1);
 		}
 		return null;
 	}
