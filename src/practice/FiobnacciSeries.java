22af9c83de3d7df1b54c561f66eff983832c25f1
ROBIN SINGH <robinsingh0210@gmail.com>
2018-11-15 11:35:05 +0530

    Fiobnacci Series using DP

--- src/practice/FiobnacciSeries.java
+++ src/practice/FiobnacciSeries.java
@@ -8,7 +8,20 @@ class FiobnacciSeries {
 	}
 
 	public static void main(String args[]) {
-		int n = 9;
+		int n = 40;
 		System.out.println(fib(n));
+		System.out.println(fibDP(n));
+	}
+
+	static int fibDP(int n) {
+		if (n == 0 || n == 1)
+			return n;
+		int []fib = new int[n+2];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[n];
 	}
 }
