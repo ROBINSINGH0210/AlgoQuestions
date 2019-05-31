819789d651e6312884497bddb75df909560f0ba2
ROBIN SINGH <robinsingh0210@gmail.com>
2019-05-31 15:39:43 +0530

    Updated Merge sort code

--- src/goldman/MergeSortTest.java
+++ src/goldman/MergeSortTest.java
@@ -3,7 +3,7 @@ package goldman;
 public class MergeSortTest {
 
 	public static void main(String[] args) {
-		int arr[] = { 4, 5, 1, 2 ,34,4,121,4,12,111};
+		int arr[] = { 4, 5, 1, 2, 34, 4, 121, 4, 12, 111 };
 
 		for (int i : mergeSort(arr, 0, arr.length - 1, new int[arr.length])) {
 			System.out.println(i);
@@ -31,18 +31,16 @@ public class MergeSortTest {
 		int k = start;
 		while (i <= j && j <= end) {
 			if (temp[i] <= temp[j]) {
-				arr[k] = temp[i];
-				i++;
+				arr[k++] = temp[i++];
 			} else {
-				arr[k] = temp[j];
-				j++;
+				arr[k++] = temp[j++];
 			}
-			k++;
 		}
 		while (i <= mid) {
-			arr[k] = temp[i];
-			i++;
-			k++;
+			arr[k++] = temp[i++];
+		}
+		while (j <= mid) {
+			arr[k++] = temp[j++];
 		}
 
 	}
