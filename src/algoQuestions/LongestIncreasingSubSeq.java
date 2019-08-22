627431dadf02380b1d54fbb4682dab6a8f790cfa
robinsingh0210 <robinsingh0210@gmail.com>
2019-08-22 17:35:40 +0530

    updated

--- src/algoQuestions/LongestIncreasingSubSeq.java
+++ src/algoQuestions/LongestIncreasingSubSeq.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class LongestIncreasingSubSeq {
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
+		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60,70,3 }; 
 		System.out.println(getLongestSq(arr));
 	}
 	
