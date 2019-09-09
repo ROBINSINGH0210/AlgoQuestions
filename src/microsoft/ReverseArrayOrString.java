3b15e35b72aaeb227d9ab559fbbeaa9e6f2f1729
robinsingh0210 <robinsingh0210@gmail.com>
2019-09-09 15:31:28 +0530

    reverse String Recursively

--- src/microsoft/ReverseArrayOrString.java
+++ src/microsoft/ReverseArrayOrString.java
@@ -3,8 +3,8 @@ package microsoft;
 public class ReverseArrayOrString {
 
 	static int[] reverse(int[] arr) {
-		int size = arr.length -1;
-		for (int i = 0; i < arr.length/2; i++) {
+		int size = arr.length - 1;
+		for (int i = 0; i < arr.length / 2; i++) {
 			int s = arr[i];
 			arr[i] = arr[size];
 			arr[size] = s;
@@ -12,11 +12,11 @@ public class ReverseArrayOrString {
 		}
 		return arr;
 	}
-	
+
 	static String reverseString(String str) {
 		char arr[] = str.toCharArray();
-		int size = arr.length -1;
-		for (int i = 0; i < arr.length/2; i++) {
+		int size = arr.length - 1;
+		for (int i = 0; i < arr.length / 2; i++) {
 			char s = arr[i];
 			arr[i] = arr[size];
 			arr[size] = s;
@@ -24,16 +24,22 @@ public class ReverseArrayOrString {
 		}
 		return new String(arr);
 	}
-	
-	
+
+	static String reverseStringRec(String orig, String rev, int i) {
+		if (i < 0 || rev.length() == orig.length())
+			return rev;
+
+		return reverseStringRec(orig, rev + orig.charAt(i), --i);
+	}
+
 	public static void main(String[] args) {
-		int arr[] = {4, 5, 1, 2};
-		reverse(arr);
-		for (int i : arr) {
-			System.out.println(i);
-		}
-		
-		System.out.println(reverseString("robin"));
+//		int arr[] = { 4, 5, 1, 2 };
+//		reverse(arr);
+//		for (int i : arr) {
+//			System.out.println(i);
+//		}
+
+		System.out.println(reverseStringRec("robin", "", "robin".length() - 1));
 	}
 
 }
