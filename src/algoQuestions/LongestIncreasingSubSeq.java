1c16dfe9b8fe365a6e07beb9acab8969415a1bf2
robin <robin@SUPERMAN>
2021-04-06 11:30:10 +0530

    Optimized code.

--- src/algoQuestions/LongestIncreasingSubSeq.java
+++ src/algoQuestions/LongestIncreasingSubSeq.java
@@ -6,28 +6,23 @@ public class LongestIncreasingSubSeq {
 
 	public static void main(String[] args) {
 		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60,70,3 }; 
-		System.out.println(getLongestSq(arr));
+		System.out.println(getLongest(arr));
 	}
 	
 	public static int getLongest(int arr[]) {
-		int max= Integer.MIN_VALUE;
-		List<Queue<Integer>> list = new  LinkedList<Queue<Integer>>();
+		Queue<Integer> q = null ;
 		for(int i =0; i< arr.length; i++) {
-			Queue<Integer> que = new PriorityQueue<Integer>(Collections.reverseOrder());
-			que.add(arr[i]);
+			Queue<Integer> temp = new PriorityQueue<>(Collections.reverseOrder());
+			temp.add(arr[i]);
 			for(int j =i+1; j< arr.length; j++) {
-				if(que.peek() < arr[j]) 
-					que.add(arr[j]);
+				if(temp.peek() < arr[j]) 
+					temp.add(arr[j]);
 			}
-			list.add(que);
+			if(q.size() < temp.size()) {
+				q = temp;
+			}
 		}
-		
-		for(Queue<Integer> quer:list) {
-			if(quer.size() > max)
-				max = quer.size();
-		}
-		
-		return max;
+		return q.size();
 		
 	}
 	
