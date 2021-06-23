12d1fd94f40d9e62e3bd9e05e6f14a0adef5e890
Robin Singh <robinsingh0210@gmail.com>
2021-06-23 13:12:13 +0530

    Reverse Words in String

--- src/algoQuestions/ReverseWordsInSentence.java
+++ src/algoQuestions/ReverseWordsInSentence.java
@@ -1,12 +1,13 @@
 package algoQuestions;
+
 public class ReverseWordsInSentence {
 
 	public static void main(String[] args) {
 		String str = "India is best";
 
 		System.out.println(reverse(str, 0, "", ""));
+		System.out.println(reverseString(str, 0, new StringBuilder(), ""));
 	}
-
 
 	static String reverse(String str, int i, String out, String temp) {
 		if (i >= str.length()) {
@@ -17,6 +18,18 @@ public class ReverseWordsInSentence {
 			return reverse(str, i + 1, out, temp + str.charAt(i));
 		else
 			return reverse(str, i + 1, temp + " " + out, "");
+
+	}
+
+	static String reverseString(String str, int i, StringBuilder out, String temp) {
+		if (i >= str.length()) {
+			return (out.append(" " + temp)).toString();
+		}
+
+		if (str.charAt(i) != ' ') {
+			return reverseString(str, i + 1, out, str.charAt(i) + temp);
+		} else
+			return reverseString(str, i + 1, (out.length()==0)? out.append(temp) : out.append(" " + temp), "");
 
 	}
 
