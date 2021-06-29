dfb8baa392cbfb0df8716bd95c258ee4a0df3daf
Robin Singh <robinsingh0210@gmail.com>
2021-06-29 15:23:33 +0530

    Used StringBuilder

--- src/algoQuestions/DecodeString.java
+++ src/algoQuestions/DecodeString.java
@@ -11,59 +11,31 @@ public class DecodeString {
 	}
 
 	static String decodeString(String str) {
-		String res = "";
-		Stack<Character> st = new Stack<>();
+		Stack<StringBuilder> st = new Stack<>();
 		Stack<Integer> st2 = new Stack<>();
 	
-
-		for (int i = 0; i < str.length(); i++) {
-			int count = 0;
-			if (Character.isDigit(str.charAt(i))) {
-				while (i < str.length() && Character.isDigit(str.charAt(i))) {
-					count = (count * 10) + Character.getNumericValue(str.charAt(i));
-					i++;
-				}
-				st2.push(count);
-			}
-			if (str.charAt(i) == '[') {
-				if (Character.isDigit(str.charAt(i - 1))) {
-					st.push(str.charAt(i));
-				} else {
-					st.push(str.charAt(i));
-					st2.push(1);
-
-				}
-
-			} else if (str.charAt(i) == ']') {
-				String temp = "";
-				while (!st.isEmpty() && st.peek() != '[') {
-					temp = st.pop() + temp;
-				}
-
-				if (!st.isEmpty() && st.peek() == '[')
-					st.pop();
-
-				int p = 0;
-				if (!st2.isEmpty())
-					p = st2.pop();
-				for (int j = 1; j <= p; j++) {
-					res += temp;
-				}
-
-				for (int k = 0; k < res.length(); k++) {
-					st.push(res.charAt(k));
-				}
-				res = "";
-
-			} else
-				st.push(str.charAt(i));
-		}
-
-		while (!st.isEmpty()) {
-			res = st.pop() + res;
-		}
-
-		return res;
+		int num = 0;
+        StringBuilder cur = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                num = num * 10 + ch - '0';
+            } else if (ch == '[') {
+                st2.push(num);
+                st.push(cur);
+                cur = new StringBuilder();
+                num = 0;
+            } else if (ch == ']') {
+                StringBuilder tmp = cur;
+                cur = st.pop();
+                int k = st2.pop();
+                for (int i = 0; i < k; i++) {
+                    cur.append(tmp);
+                }
+            } else {
+                cur.append(ch);
+            }
+        }
+        return cur.toString();
 	}
 
 }
