19c62945a0c8a586ecd4747888c031e93f4b24fd
Robin Singh <robinsingh0210@gmail.com>
2021-07-07 23:59:12 +0530

    Issue fixed

--- src/algoQuestions/PhoneNumberCode.java
+++ src/algoQuestions/PhoneNumberCode.java
@@ -5,25 +5,27 @@ import java.util.*;
 public class PhoneNumberCode {
 
 	public static void main(String[] args) {
-		System.out.println(letterCombinations("534"));
+		System.out.println(letterCombinations("12"));
 
 	}
-	public static List<String> letterCombinations(String digits) {  
-	    if(digits==null) return null;  
-	    List<String> res= new ArrayList<String>();  
-	    res.add("");  
-	    String[] letters={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};  
-	    for(int i=0;i<digits.length();i++){  
-	      String letter=letters[digits.charAt(i)-'2'];  
-	      List<String> newRes=new ArrayList<String>();  
-	      for(int j=0; j<letter.length();j++){  
-	        for(String s: res){  
-	          s+=letter.charAt(j);  
-	          newRes.add(s);  
-	        }  
-	      }  
-	      res=newRes;  
-	    }  
-	    return res;  
-	   }  
+
+	public static List<String> letterCombinations(String digits) {
+		if (digits == null)
+			return null;
+		List<String> res = new ArrayList<String>();
+		res.add("");
+		String[] letters = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+		for (int i = 0; i < digits.length(); i++) {
+			String letter = letters[digits.charAt(i) - '1'];
+			List<String> newRes = new ArrayList<String>();
+			for (int j = 0; j < letter.length(); j++) {
+				for (String s : res) {
+					s += letter.charAt(j);
+					newRes.add(s);
+				}
+			}
+			res = newRes;
+		}
+		return res;
+	}
 }
