eb1684b2d02e126d82970dde70662160cf6f60af
Robin Singh <robinsingh0210@gmail.com>
2021-07-13 12:18:15 +0530

    three Sum Equal Zero

--- src/arrayAlgoQues/FindTripletWithGivenSum.java
+++ src/arrayAlgoQues/FindTripletWithGivenSum.java
@@ -1,6 +1,7 @@
 package arrayAlgoQues;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -10,9 +11,10 @@ import java.util.*;
 public class FindTripletWithGivenSum {
 
 	public static void main(String[] args) {
-		int arr[] = new int[] { 3, 6, 10, 11 };
+		int arr[] = new int[] { -1,0,1,2,-1,-4 };
 
-		System.out.println(findTriplet(arr, 20));
+//		System.out.println(findTriplet(arr, 20));
+		System.out.println(threeSumEqualZero(arr));
 	}
 
 	static List<Integer> findTriplet(int arr[], int value) {
@@ -35,5 +37,40 @@ public class FindTripletWithGivenSum {
 		}
 
 		return list;
+	}
+	
+	public static List<List<Integer>> threeSumEqualZero(int[] nums) {
+
+		if (nums.length < 3) {
+			return Collections.emptyList();
+		}
+
+		Set<List<Integer>> set = new HashSet<>();
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0)
+				break;
+			if (i != 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+			int target = -nums[i];
+			int j = i + 1;
+			int k = nums.length - 1;
+
+			while (j < k) {
+				if (nums[j] + nums[k] == target) {
+					set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+					j++;
+					k--;
+				} else if (nums[j] + nums[k] > target) {
+					k--;
+				} else {
+					j++;
+				}
+			}
+		}
+
+		return set.stream().collect(Collectors.toList());
 	}
 }
