10e7362225774e2e7b22628520893e708b8cdee3
Robin Singh <robinsingh0210@gmail.com>
2021-07-26 17:35:25 +0530

    canPartitionInEqualSubSet

--- src/arrayAlgoQues/SubsetSumProblem.java
+++ src/arrayAlgoQues/SubsetSumProblem.java
@@ -6,12 +6,12 @@ public class SubsetSumProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[] = { 1, 2, 3, 5 };
+		int arr[] = { 2, 3, 5, 6, 8, 10 };
 		SubsetSumProblem s = new SubsetSumProblem();
-		System.out.println(s.canPartitionInEqualSubSet(arr));
+		System.out.println(s.findCountDP(arr, arr.length, 10));
 	}
 
-	int isSubsetPresent(int[] arr, int i, int sum) {
+	int findCount(int[] arr, int i, int sum) {
 		if (sum == 0) {
 			return 1;
 		}
@@ -19,9 +19,27 @@ public class SubsetSumProblem {
 			return 0;
 		}
 
-		int sub = isSubsetPresent(arr, i - 1, sum);
-		int sub1 = isSubsetPresent(arr, i - 1, sum - arr[i - 1]);
+		int sub = findCount(arr, i - 1, sum);
+		int sub1 = findCount(arr, i - 1, sum - arr[i - 1]);
 		return sub + sub1;
+	}
+
+	int findCountDP(int[] arr, int n, int sum) {
+		int[][] sub = new int[sum + 1][n + 1];
+		for (int i = 0; i < sub[0].length; i++) {
+			sub[0][i] = 1;
+		}
+
+		for (int i = 1; i <= sum; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i >= arr[j - 1]) {
+					sub[i][j] = sub[i][j - 1] + sub[i - arr[j - 1]][j - 1];
+				} else
+					sub[i][j] += sub[i][j - 1];
+			}
+		}
+
+		return sub[sum][n];
 	}
 
 	boolean isSUbSet(int[] arr, int n, int sum) {
