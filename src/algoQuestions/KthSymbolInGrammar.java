0ed9cb5b21da928e2d37b0f9746dfb19a953ba0e
Superman <Superman@SUPERMAN>
2022-03-23 14:17:52 +0530

    KthSymbolInGrammar

---
+++ src/algoQuestions/KthSymbolInGrammar.java
@@ -0,0 +1,21 @@
+package algoQuestions;
+
+public class KthSymbolInGrammar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(kthGrammar(2, 2));
+	}
+
+	public static int kthGrammar(int n, int k) {
+		if (n == 1 && k == 1) {
+			return 0;
+		}
+		int mid = (int) (Math.pow(2, n - 1) / 2);
+		if (k <= mid) {
+			return kthGrammar(n - 1, k);
+		} else
+			return (kthGrammar(n - 1, k - mid) == 0) ? 1 : 0;
+
+	}
+}
